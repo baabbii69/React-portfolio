67e5cd33c136eb24b6cad7d25ba587d1d388305d
baabbii <87692056+baabbii69@users.noreply.github.com>
2022-08-02 20:18:00 +0300

    Logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
+import { motion } from "framer-motion";
 import React, { useState } from "react";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
-import { motion } from "framer-motion";
 
 import { images } from "../../constants";
 import "./Navbar.scss";
@@ -11,7 +11,9 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
+        <a href="#home">
+          <img src={images.logo} alt="logo" />
+        </a>
       </div>
       <ul className="app__navbar-links">
         {["home", "about", "work", "skills", "contact"].map((item) => (
